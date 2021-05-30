3c939d6fb49516c5839bb11cb6c2542de9e1b54d
hyoni0817 <dudu1104@naver.com>
2021-05-31 03:52:57 +0900

    Update: 한 번만 뒤집기 위해서 add로 변경

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const card = document.querySelectorAll(".card__inner");
 
 for(let i = 0; i < card.length; i++) {
   card[i].addEventListener("click", function (e) {
-    card[i].classList.toggle('is-flipped'); 
+    card[i].classList.add('is-flipped'); 
+    //반복해서 뒤집고 싶을 때는 add대신에 toggle
   }, {
     capture: true,
   });
